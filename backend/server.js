0c04831011ab87d3a90549489ec583140306935c
Saurabh Sircar <saurabhsircar11@gmail.com>
2025-05-04 22:12:56 +0530

    feat: added dev hot reloading

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,39 @@
-const express= require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
-const cors= require('cors');
+const cors = require("cors");
 
-const app =  express();
+const app = express();
 const port = 3000;
 
-const DEV_URL= 'http://flask-api:5000/translate'
+const DEV_URL = "http://flask-api:5000/translate";
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/translate', async (req,res)=>{
-    const { texts, source_lang, target_lang, context}= req.body;
+app.post("/translate", async (req, res) => {
+  const { texts, source_lang, target_lang, context } = req.body;
 
-    try{
-        const response = await axios.post(DEV_URL,{ texts, source_lang, target_lang, context});
-        const {results} = response.data
-        const translations= results.map(({translated})=> translated)
+  try {
+    const response = await axios.post(DEV_URL, {
+      texts,
+      source_lang,
+      target_lang,
+      context,
+    });
+    const { results } = response.data;
+    const translations = results.map(({ translated }) => translated);
 
-        res.json({translations});
-    }catch(error){
-        res.status(500).json({error: error.message});
-    }
-})
+    res.json({ translations });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Worker Started, PID:${process.pid}`);
-    console.log({ environment: 'dev'});
-    console.log(`Server is running on port ${port}`);
-    console.log(`Server running at http://localhost:${port}`);
-  });
+  console.log(`Worker Started, PID:${process.pid}`);
+  console.log({ environment: "dev" });
+  console.log(`Server is running on port ${port}`);
+  console.log(`Server running at http://localhost:${port}`);
+});
--- flask-api/app.py
+++ flask-api/app.py
@@ -96,4 +96,4 @@ def shutdown():
         t.join()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
